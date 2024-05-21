99612cdaa8bb74b6348160dbe6393e365bb684c9
GerardCaja <120013160+GerardCaja@users.noreply.github.com>
2024-05-21 10:59:15 +0200

    lññl

--- Assets/Puzzle/Scripts/GameManager.cs
+++ Assets/Puzzle/Scripts/GameManager.cs
@@ -50,7 +50,7 @@ public class GameManager : MonoBehaviour {
   // Start is called before the first frame update
   void Start() {
     pieces = new List<Transform>();
-    size = 2;
+    size = 3;
     CreateGamePieces(0.01f);
   }
 
